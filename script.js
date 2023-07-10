0dca6072faed550eb653a22a7627db7ce7a219af
frog <136676079+frogrin@users.noreply.github.com>
2023-07-10 14:57:15 +0300

    Create getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello, world")
+// Write a function that will randomly return "rock" "paper" "scissors"
+    // First we need a function that will return one of 3 numbers randomly
+    // Then we will have to assing each of these numbers to a text value
+
+
+function random() {
+    return Math.floor(Math.random() * 3);
+}
+
+function getComputerChoice() {
+    let num = random();
+    switch (num) {
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2: 
+            return "Scissors";
+            break;
+    }
+}
