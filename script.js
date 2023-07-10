2d0e429a9cb75aff5879e559d1dda3710b4620b3
frog <136676079+frogrin@users.noreply.github.com>
2023-07-10 15:19:13 +0300

    Create a function that plays a round of the game

--- script.js
+++ script.js
@@ -33,3 +33,42 @@ function getPlayerChoice() {
     return choice;
 }
 // Then we will write a function that takes two parameters and decides on a winner and displays a message
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock") {
+        switch (computerSelection) {
+            case "rock":
+                return "It's a tie!";
+                break;
+            case "paper":
+                return "You lose! Paper beats Rock";
+                break;
+            case "scissors":
+                return "You win! Rock beats Scissors!";
+                break;
+        }
+    } else if (playerSelection === "paper") {
+        switch (computerSelection) {
+            case "rock":
+                return "You win! Paper beats Rock";
+                break;
+            case "paper":
+                return "It's a tie!";
+                break;
+            case "scissors":
+                return "You lose! Scissors beat Paper";
+                break;
+        }
+    } else if (playerSelection === "scissors") {
+        switch (computerSelection) {
+            case "rock":
+                return "You lose! Rock beats Scissors";
+                break;
+            case "paper":
+                return "You win! Scissors beat Paper";
+                break;
+            case "scissors":
+                return "It's a tie!";
+                break;
+        }
+    }
+}
