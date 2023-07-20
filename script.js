c9b4abaf8576f14227b2028ca9ff63f50889d3bd
frog <136676079+frogrin@users.noreply.github.com>
2023-07-20 11:27:39 +0300

    Add function that displays current score and the winner

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const result = document.querySelector(".result");
+const score = document.querySelector(".score");
 
 const buttonRock = document.querySelector("#rock");
 buttonRock.addEventListener('click', () => playRound('rock', getComputerChoice()));
@@ -8,6 +9,9 @@ buttonPaper.addEventListener('click', () => playRound('paper', getComputerChoice
 
 const buttonScissors = document.querySelector("#scissors");
 buttonScissors.addEventListener('click', () => playRound('scissors', getComputerChoice()));
+
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
@@ -19,6 +23,16 @@ function getPlayerChoice() {
     choice = prompt("Your choice: ").toLowerCase();
     return choice;
 }
+
+function updateScore() {
+  if (playerScore > computerScore && playerScore === 5) {
+    score.textContent = `The Player won with the score ${playerScore}:${computerScore}`;
+  } else if (playerScore < computerScore && computerScore === 5) {
+    score.textContent = `The Computer won with the score ${playerScore}:${computerScore}`;
+  } else {
+    score.textContent = `The score is ${playerScore}:${computerScore}`;
+  }
+} 
 // Then we will write a function that takes two parameters and decides on a winner and displays a message
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock") {
@@ -28,34 +42,42 @@ function playRound(playerSelection, computerSelection) {
                 break;
             case "paper":
                 result.textContent = "You lose! Paper beats Rock";
+                computerScore++;
                 break;
             case "scissors":
                 result.textContent = "You win! Rock beats Scissors!";
+                playerScore++;
                 break;
         }
     } else if (playerSelection === "paper") {
         switch (computerSelection) {
             case "rock":
                 result.textContent = "You win! Paper beats Rock";
+                playerScore++;
                 break;
             case "paper":
                 result.textContent = "It's a tie!";
                 break;
             case "scissors":
                 result.textContent = "You lose! Scissors beat Paper";
+                computerScore++;
                 break;
         }
     } else if (playerSelection === "scissors") {
         switch (computerSelection) {
             case "rock":
                 result.textContent = "You lose! Rock beats Scissors";
+                computerScore++;
                 break;
             case "paper":
                 result.textContent = "You win! Scissors beat Paper";
+                playerScore++;
                 break;
             case "scissors":
                 result.textContent = "It's a tie!";
                 break;
         }
     }
+
+    updateScore();
 }
